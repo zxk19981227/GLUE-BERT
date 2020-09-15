c6af5597c018c2f84b1ffc497f8a267c284eb3af
zhouxukun <xukun_zhou@shannonai.com>
2020-09-15 15:08:02 +0800

    增加了运行文件run.sh‘ ’

---
+++ source/run.sh
@@ -0,0 +1,10 @@
+pip install requirements.txt
+python main.py --Task CoLA 
+python main.py --Task SST-2
+python main.py --Task RTE
+python main.py --Task MNLI
+python main.py --Task MRPC
+python main.py --Task QNLI
+python main.py --Task QQP
+python main.py --Task STS-B
+python main.py --Task SNLI
